1ed70df379785c2b2c901b56eea1050cdc95541b
Eugene Chu <eugenchu@gmail.com>
2021-04-14 15:40:37 -0700

    update the port to take either process.env.port or 3030

--- server/index.js
+++ server/index.js
@@ -2,11 +2,12 @@ const express = require ('express');
 const path = require('path');
 
 const app = express();
+const PORT = process.env.PORT || 3030;
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.listen(3030, () => {
-  console.log('Server listening at port 3030');
+app.listen(PORT, () => {
+  console.log(`Server listening at port ${PORT}`);
 })
